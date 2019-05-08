f26a74c631aede92b4a322d0402216bcba3bcc38
Adrian Petrescu <apetresc@gmail.com>
2019-05-08 01:03:12 -0400

    Bring back config.php
    It's safe now, all secrets are in production.env

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'mysql';
+$dbport = '';
+$dbname = getenv('MYSQL_DATABASE');
+$dbuser = getenv('MYSQL_USER');
+$dbpasswd = getenv('MYSQL_PASSWORD');
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
